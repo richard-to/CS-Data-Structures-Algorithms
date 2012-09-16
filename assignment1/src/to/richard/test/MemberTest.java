8de20d7a06e0dffc6b10e4c8b21877a6f3058516
richard-to <gohn76@gmail.com>
2012-09-15 21:49:47 -0800

    Update member constructors in tests

--- assignment1/src/to/richard/test/MemberTest.java
+++ assignment1/src/to/richard/test/MemberTest.java
@@ -8,32 +8,32 @@ import to.richard.Member;
 public class MemberTest {
     @Test
     public void testGetStanding() throws Exception {
-        Member member = new Member(0.0f, 0.0f, "Bob", "Fr");
+        Member member = new Member(0.0f, 0.0f, "Fr", "Bob");
         assertEquals("Fr", member.getStanding());
     }
 
     @Test
     public void testGetName() throws Exception {
-        Member member = new Member(0.0f, 0.0f, "Bob", "Fr");
+        Member member = new Member(0.0f, 0.0f, "Fr", "Bob");
         assertEquals("Bob", member.getName());
     }
 
     @Test
     public void testGetPointsEarned() throws Exception {
-        Member member = new Member(8.6f, 2.6f, "Bob", "Fr");
+        Member member = new Member(8.6f, 2.6f, "Fr", "Bob");
         assertEquals(2.6f, member.getPointsEarned(), 0.0);
     }
 
     @Test
     public void testGetHoursAttempted() throws Exception {
-        Member member = new Member(1.5f, 4.5f, "Bob", "Fr");
+        Member member = new Member(1.5f, 4.5f, "Fr", "Bob");
         assertEquals(1.5f, member.getHoursAttempted(), 0.0);
     }
 
     @Test
     public void testEquals() throws Exception {
-        Member member1 = new Member(1.5f, 4.5f, "Bob", "Fr");
-        Member member2 = new Member(1.5f, 4.5f, "Bob", "Fr");
+        Member member1 = new Member(1.5f, 4.5f, "Fr", "Bob");
+        Member member2 = new Member(1.5f, 4.5f, "Fr", "Bob");
 
         assertEquals(true, member1.equals(member1));
         assertEquals(true, member1.equals(member2));
@@ -41,8 +41,8 @@ public class MemberTest {
 
     @Test
     public void testNotEquals() throws Exception {
-        Member member1 = new Member(1.5f, 4.5f, "Bob", "So");
-        Member member2 = new Member(1.5f, 4.5f, "Bob", "Fr");
+        Member member1 = new Member(1.5f, 4.5f, "So", "Bob");
+        Member member2 = new Member(1.5f, 4.5f, "Fr", "Bob");
         assertEquals(false, member1.equals(member2));
     }
 }
