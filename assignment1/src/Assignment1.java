651f385566c9abcf5d6ac267efe4ac76e78b3367
richard-to <gohn76@gmail.com>
2012-09-15 22:25:53 -0800

    Add test to check if new users added

--- assignment1/src/Assignment1.java
+++ assignment1/src/Assignment1.java
@@ -5,12 +5,14 @@ import to.richard.MemberReader;
 public class Assignment1 {
 
     private static final String UHC_FILE = "UHC.dat";
-    private static final String NEW_FILE = "new.data";
+    private static final String NEW_FILE = "new.dat";
     private static final String GRADES_FILE = "grades.dat";
 
     public static void main(String[] args) {
         MemberReader reader = new MemberReader();
         MemberList memberList = reader.readCurrent(UHC_FILE);
+        System.out.print(memberList.toString());
+        memberList = reader.readNew(memberList, NEW_FILE);
         System.out.print(memberList.toString());
     }
 }
