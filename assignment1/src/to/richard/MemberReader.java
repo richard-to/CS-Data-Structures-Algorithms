87ab43ec06f60fef0a16199287839d710887d148
richard-to <gohn76@gmail.com>
2012-09-15 21:50:36 -0800

    Add member reader

---
+++ assignment1/src/to/richard/MemberReader.java
@@ -0,0 +1,31 @@
+package to.richard;
+
+import java.io.*;
+
+public class MemberReader {
+
+    private static final String DELIM = " ";
+
+    public MemberList readCurrent(String filePath){
+        File file = new File(filePath);
+        MemberList memberList = new MemberList();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] data = line.split(DELIM);
+                memberList.add(new Member(
+                        Float.parseFloat(data[0]),
+                        Float.parseFloat(data[1]),
+                        data[2],
+                        data[3]));
+            }
+        } catch(FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return memberList;
+    }
+}
