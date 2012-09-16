6e1568cc3bcd7fcf2e0a313319a70048fa80799f
richard-to <gohn76@gmail.com>
2012-09-15 21:51:07 -0800

    Add code to read UHC.dat file

--- assignment1/src/Assignment1.java
+++ assignment1/src/Assignment1.java
@@ -1,8 +1,15 @@
+import to.richard.LinkedList;
 import to.richard.Member;
+import to.richard.MemberReader;
 
 public class Assignment1 {
 
-    public static void main(String[] args) {
+    private static final String UHC_FILE = "UHC.dat";
+    private static final String NEW_FILE = "new.data";
+    private static final String GRADES_FILE = "grades.dat";
 
+    public static void main(String[] args) {
+        MemberReader reader = new MemberReader();
+        LinkedList<Member> memberList = reader.readCurrent(UHC_FILE);
     }
 }
