9ea647076c5fba23d8a1c442c867f8e85f05e5cc
richard-to <gohn76@gmail.com>
2012-09-16 11:43:55 -0800

    Add constants for Standing and Points Earned. Add updateStanding method.

--- assignment1/src/to/richard/Member.java
+++ assignment1/src/to/richard/Member.java
@@ -1,13 +1,24 @@
 package to.richard;
 
 public class Member {
+
+    public static final String FRESHMAN = "Fr";
+    public static final String SOPHOMORE = "So";
+    public static final String JUNIOR = "Jr";
+    public static final String SENIOR = "Sr";
+
+    public static final float FR_STAND = 0.0f;
+    public static final float SO_STAND = 30.0f;
+    public static final float JR_STAND = 60.0f;
+    public static final float SR_STAND = 90.0f;
+
     private float hoursAttempted;
     private float pointsEarned;
     private String name;
     private String standing;
 
     public Member(String name) {
-        this(0.0f, 0.0f, "Fr", name);
+        this(0.0f, 0.0f, FRESHMAN, name);
     }
 
     public Member(float hoursAttempted, float pointsEarned, String standing, String name) {
@@ -17,9 +28,22 @@ public class Member {
         this.standing = standing;
     }
 
-    public void updateGrades(Float hoursAttempted, Float pointsEarned){
+    public Member updateGrades(Float hoursAttempted, Float pointsEarned){
         this.hoursAttempted += hoursAttempted;
         this.pointsEarned += pointsEarned;
+        return this;
+    }
+
+    public Member updateStanding(){
+        if(pointsEarned >= SR_STAND)
+            standing = SENIOR;
+        else if(pointsEarned >= JR_STAND)
+            standing = JUNIOR;
+        else if(pointsEarned >= SO_STAND)
+            standing = SOPHOMORE;
+        else
+            standing = FRESHMAN;
+        return this;
     }
 
     public boolean isNamed(String name){
