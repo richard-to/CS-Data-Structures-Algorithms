7760f64f150db1d6960f58c15f3a746081c2a186
richard-to <gohn76@gmail.com>
2012-09-16 21:33:38 -0800

    Add accept input after each section. Add headers to sections.

--- assignment1/src/Assignment1.java
+++ assignment1/src/Assignment1.java
@@ -1,18 +1,57 @@
-import to.richard.MemberList;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import to.richard.MemberManager;
 
 public class Assignment1 {
 
+    private static final String PROMPT = "Press 'Enter' to continue.";
+    private static final String HORIZONTAL_RULE = "--------------------------";
     private static final String UHC_FILE = "UHC.dat";
     private static final String NEW_FILE = "new.dat";
     private static final String GRADES_FILE = "grades.dat";
 
     public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         MemberManager manager = new MemberManager();
-        manager.loadCurrent(UHC_FILE).print();
-        manager.loadNew(NEW_FILE).print();
-        manager.loadNewGrades(GRADES_FILE).print();
-        manager.sortByPoints().print();
-        manager.dropUnderPerforming().printGPA();
+
+        try {
+            System.out.println("Current Members");
+            System.out.println(HORIZONTAL_RULE);
+            manager.loadCurrent(UHC_FILE).print();
+            System.out.println(PROMPT);
+            br.readLine();
+
+            System.out.println("Updated Members List");
+            System.out.println(HORIZONTAL_RULE);
+            manager.loadNew(NEW_FILE).print();
+            System.out.println(PROMPT);
+            br.readLine();
+
+            System.out.println("Updated Grades");
+            System.out.println(HORIZONTAL_RULE);
+            manager.loadNewGrades(GRADES_FILE).print();
+            System.out.println(PROMPT);
+            br.readLine();
+
+            System.out.println("Members Ranked By Credits Earned");
+            System.out.println(HORIZONTAL_RULE);
+            manager.sortByPoints().print();
+            System.out.println(PROMPT);
+            br.readLine();
+
+            System.out.println("Dropped Members List");
+            System.out.println(HORIZONTAL_RULE);
+            manager.dropUnderPerforming();
+
+            System.out.println("Member GPA List");
+            System.out.println(HORIZONTAL_RULE);
+            manager.printGPA();
+
+        } catch(IOException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
