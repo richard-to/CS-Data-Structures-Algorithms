f0d47f7028ed4f806c022e5cfcc8374da5c63bee
richard-to <gohn76@gmail.com>
2012-09-20 22:03:10 -0800

    Add worst case benchmark

--- assignment1/src/BenchmarkLinkedList.java
+++ assignment1/src/BenchmarkLinkedList.java
@@ -3,11 +3,18 @@ import to.richard.Member;
 
 public class BenchmarkLinkedList {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 3; i++){
             System.out.println("Run " + i);
             benchmark(1000);
             benchmark(10000);
             benchmark(100000);
+        }
+
+        for(int i = 0; i < 3; i++){
+            System.out.println("Run " + i);
+            benchmarkWorst(1000);
+            benchmarkWorst(10000);
+            benchmarkWorst(100000);
         }
     }
 
@@ -16,6 +23,17 @@ public class BenchmarkLinkedList {
         long t1 = System.nanoTime();
         for(int i = 0; i < count; i++){
             list.add(new Member("name" + i));
+        }
+        long t2 = System.nanoTime();
+        System.out.format("Count: %-7d Time: %d ns %n", count, (t2 - t1));
+    }
+
+    public static void benchmarkWorst(int count){
+        LinkedList<Member> list = new LinkedList<Member>();
+        long t1 = System.nanoTime();
+        for(int i = 0; i < count; i++){
+            int size = (list.size() > 0) ? list.size()-1 : 0;
+            list.add(size, new Member("name" + i));
         }
         long t2 = System.nanoTime();
         System.out.format("Count: %-7d Time: %d ns %n", count, (t2 - t1));
