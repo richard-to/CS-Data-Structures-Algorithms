dc2d9df320def2766cd38135843a056836a182c7
richard-to <gohn76@gmail.com>
2012-09-23 11:30:39 -0800

    Add comments for iterator interface

--- assignment1/src/to/richard/Iterator.java
+++ assignment1/src/to/richard/Iterator.java
@@ -1,5 +1,12 @@
 package to.richard;
 
+/**
+ * Customer iterator interface
+ *
+ * Does not implement remove method.
+ *
+ * @param <E>
+ */
 public interface Iterator<E> {
     public boolean hasNext();
     public E next();
