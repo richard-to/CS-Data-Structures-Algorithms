e2196bca801530b896dcc70f5c38cc56c9c11d6f
richard-to <gohn76@gmail.com>
2012-09-23 10:30:24 -0800

    Add comments for comparor class

--- assignment1/src/to/richard/MemberSortByPoints.java
+++ assignment1/src/to/richard/MemberSortByPoints.java
@@ -2,7 +2,19 @@ package to.richard;
 
 import java.util.Comparator;
 
+/**
+ * Comparator class to sort by points earned.
+ */
 public class MemberSortByPoints implements Comparator<Member> {
+
+    /**
+     * Compares members by points earned.
+     *
+     * @param member1
+     * @param member2
+     *
+     * @return  0 if equal, 1 if member1 greater, -1 if member2 greater
+     */
     public int compare(Member member1, Member member2) {
         if(member1.getPointsEarned() > member2.getPointsEarned()){
             return 1;
