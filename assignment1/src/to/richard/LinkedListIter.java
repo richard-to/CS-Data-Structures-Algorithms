c0f05deca1b856291d07c0b890ab9b32328a3429
richard-to <gohn76@gmail.com>
2012-09-23 11:31:54 -0800

    Add linked list iter

--- assignment1/src/to/richard/LinkedListIter.java
+++ assignment1/src/to/richard/LinkedListIter.java
@@ -2,6 +2,13 @@ package to.richard;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+/**
+ * LinkedList Iterator implementation
+ *
+ * Does not implement remove method.
+ *
+ * @param <E>
+ */
 public class LinkedListIter<E> implements Iterator<E> {
 
     private LinkedListNode<E> node;
@@ -10,6 +17,10 @@ public class LinkedListIter<E> implements Iterator<E> {
         this.node = node;
     }
 
+    /**
+     * Checks if another node exists in list
+     * @return boolean true|false if next node exists
+     */
     public boolean hasNext(){
         if(this.node == null)
             return false;
@@ -17,6 +28,16 @@ public class LinkedListIter<E> implements Iterator<E> {
             return true;
     }
 
+    /**
+     * Gets next node
+     *
+     * If no next node, then return null, otherwise return element.
+     *
+     * This method will set the next node in the list after returning
+     * the current node.
+     *
+     * @return E|null
+     */
     public E next(){
         if(hasNext()){
             LinkedListNode<E> current = node;
