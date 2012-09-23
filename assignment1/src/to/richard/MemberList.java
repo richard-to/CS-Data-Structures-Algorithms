d91a3ab6fb59d39583e4fca67c301284500b1702
richard-to <gohn76@gmail.com>
2012-09-23 10:23:39 -0800

    Add comments for member list

--- assignment1/src/to/richard/MemberList.java
+++ assignment1/src/to/richard/MemberList.java
@@ -2,23 +2,48 @@ package to.richard;
 
 import java.util.Comparator;
 
+/**
+ * List of members that uses DoubleLinked list internally.
+ */
 public class MemberList {
 
     private LinkedList<Member> members;
     private MemberSort memberSort;
+
+    /**
+     * Member list constructor
+     */
     public MemberList(){
         this.members = new LinkedList<Member>();
         this.memberSort = new MemberSort();
     }
 
+    /**
+     * Member list constructor
+     *
+     * @param members Create with existing linked list
+     */
     public MemberList(LinkedList<Member> members){
         this.members = members;
     }
 
+    /**
+     * Gets number of members in list
+     *
+     * @return int number of members in list
+     */
     public int size() {
         return this.members.size();
     }
 
+    /**
+     * Adds member to list
+     *
+     * Only adds unique members
+     *
+     * @param member
+     * @return boolean true|false depending if member added
+     */
     public boolean add(Member member){
         if(!contains(member)){
             this.members.add(member);
@@ -27,10 +52,22 @@ public class MemberList {
             return false;
     }
 
+    /**
+     * Checks if member is in list
+     *
+     * @param member
+     * @return boolean true|false if in member in list
+     */
     public boolean contains(Member member){
         return this.members.contains(member);
     }
 
+    /**
+     * Finds member in list by name
+     *
+     * @param name
+     * @return Member if exists or null if not exists
+     */
     public Member find(String name) {
         LinkedListIter<Member> iter = this.members.iterator();
         while(iter.hasNext()){
@@ -41,6 +78,11 @@ public class MemberList {
         return null;
     }
 
+    /**
+     * Updates standings for each member in list
+     *
+     * @return MemberList
+     */
     public MemberList updateStandings() {
         LinkedListIter<Member> iter = this.members.iterator();
         while(iter.hasNext()){
@@ -50,15 +92,25 @@ public class MemberList {
         return this;
     }
 
+    /**
+     * Sorts member list
+     *
+     * @param compare Field to sort by via comparator
+     * @return MemberList
+     */
     public MemberList sort(Comparator<Member> compare) {
         members = memberSort.sort(members, compare);
         return this;
     }
 
+    /**
+     * Drops under performing members from list
+     *
+     * @return MemberList List of dropped members
+     */
     public MemberList dropUnderPerforming() {
         MemberList dropList = new MemberList();
         LinkedListIter<Member> iter = this.members.iterator();
-        StringBuffer out = new StringBuffer();
         while(iter.hasNext()){
             Member member = iter.next();
             if(member.isUnderPerforming()){
@@ -70,6 +122,11 @@ public class MemberList {
         return dropList;
     }
 
+    /**
+     * Prints GPA of members
+     *
+     * @return String of member GPAs
+     */
     public String printGPA() {
         LinkedListIter<Member> iter = this.members.iterator();
         StringBuffer out = new StringBuffer();
@@ -83,6 +140,11 @@ public class MemberList {
         return out.toString().trim();
     }
 
+    /**
+     * Prints members as formatted in file
+     *
+     * @return String members as formatted in file
+     */
     public String toString(){
         LinkedListIter<Member> iter = this.members.iterator();
         StringBuffer out = new StringBuffer();
