09fdb7f8cd885d733413a70cdc92fd69ded22dce
richard-to <gohn76@gmail.com>
2012-09-23 10:23:25 -0800

    Add comments for member class

--- assignment1/src/to/richard/Member.java
+++ assignment1/src/to/richard/Member.java
@@ -20,10 +20,25 @@ public class Member {
     private String standing;
     private float gpa;
 
+    /**
+     * Creates new Freshman
+     *
+     * @param name Name of freshman
+     */
     public Member(String name) {
         this(0.0f, 0.0f, FRESHMAN, name);
     }
 
+    /**
+     * Creates member with all data
+     *
+     * GPA calculated.
+     *
+     * @param hoursAttempted
+     * @param pointsEarned
+     * @param standing
+     * @param name
+     */
     public Member(float hoursAttempted, float pointsEarned, String standing, String name) {
         this.hoursAttempted = hoursAttempted;
         this.pointsEarned = pointsEarned;
@@ -33,12 +48,26 @@ public class Member {
         calculateGPA();
     }
 
+    /**
+     * Updates grades
+     *
+     * @param hoursAttempted
+     * @param pointsEarned
+     * @return Member
+     */
     public Member updateGrades(Float hoursAttempted, Float pointsEarned){
         this.hoursAttempted += hoursAttempted;
         this.pointsEarned += pointsEarned;
         return this;
     }
 
+    /**
+     * Updates standing of member.
+     *
+     * Standing determined by points earned
+     *
+     * @return Member
+     */
     public Member updateStanding(){
         if(pointsEarned >= SR_STAND)
             standing = SENIOR;
@@ -51,6 +80,15 @@ public class Member {
         return this;
     }
 
+    /**
+     * Calculates GPA.
+     *
+     * GPA calculated by points earned by hours attempted
+     *
+     * If hours attempted is 0, GPA will be 0.
+     *
+     * @return Member
+     */
     public Member calculateGPA()
     {
         if(hoursAttempted == 0.0f){
@@ -61,16 +99,31 @@ public class Member {
         return this;
     }
 
+    /**
+     * Returns calculated GPA
+     *
+     * @return float GPA
+     */
     public float getGPA()
     {
         return gpa;
     }
 
+    /**
+     * Checks member is under performing
+     *
+     * @return boolean
+     */
     public boolean isUnderPerforming()
     {
         return (gpa < GPA_BORDER);
     }
 
+    /**
+     * Checks if member has the same name
+     * @param name
+     * @return boolean true|false if name exists in list
+     */
     public boolean isNamed(String name){
         return this.name.equals(name);
     }
@@ -95,6 +148,13 @@ public class Member {
         return pointsEarned;
     }
 
+    /**
+     * Sets points earned
+     *
+     * Also recalculates GPA
+     *
+     * @param pointsEarned
+     */
     public void setPointsEarned(float pointsEarned) {
         this.pointsEarned = pointsEarned;
         calculateGPA();
@@ -104,16 +164,36 @@ public class Member {
         return hoursAttempted;
     }
 
+    /**
+     * Sets hours attempted
+     *
+     * Also recalculateds GPA
+     *
+     * @param hoursAttempted
+     */
     public void setHoursAttempted(float hoursAttempted) {
         this.hoursAttempted = hoursAttempted;
         calculateGPA();
     }
 
+    /**
+     * Prints member in format of single line from file
+     *
+     * @return String
+     */
     public String toString() {
         return this.hoursAttempted + " " + this.pointsEarned +
                 " " + this.standing + " " + this.name;
     }
 
+    /**
+     * Override equals method for member
+     *
+     * Equality determined by name
+     *
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
