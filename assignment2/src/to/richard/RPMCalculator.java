89f9814d03fea88fa41b0a9e900b9fa4312da2f5
richard-to <gohn76@gmail.com>
2012-10-05 15:13:37 -0800

    Add calc method to RPM calculator

--- assignment2/src/to/richard/RPMCalculator.java
+++ assignment2/src/to/richard/RPMCalculator.java
@@ -4,4 +4,41 @@ package to.richard;
  * Calculates RPM input
  */
 public class RPMCalculator {
+
+    public double calc(String input) throws DivideByZeroException {
+        Stack<Double> operandStack = new Stack<Double>();
+        String[] tokens = input.split(" ");
+
+        for(String token:tokens){
+            String trimmedToken = token.trim();
+            if(!trimmedToken.isEmpty()){
+                if(trimmedToken.matches("[0-9]+")){
+                    operandStack.push(new Double(trimmedToken));
+                } else if(trimmedToken.matches("sqrt")){
+                    Double operator = operandStack.topAndPop();
+                    operandStack.push(Math.sqrt(operator));
+                } else if(trimmedToken.matches("\\+")){
+                    Double operator2 = operandStack.topAndPop();
+                    Double operator1 = operandStack.topAndPop();
+                    operandStack.push(operator1 + operator2);
+                } else if(trimmedToken.matches("\\-")){
+                    Double operator2 = operandStack.topAndPop();
+                    Double operator1 = operandStack.topAndPop();
+                    operandStack.push(operator1 - operator2);
+                } else if(trimmedToken.matches("\\*")){
+                    Double operator2 = operandStack.topAndPop();
+                    Double operator1 = operandStack.topAndPop();
+                    operandStack.push(operator1 * operator2);
+                } else if(trimmedToken.matches("/")){
+                    Double operator2 = operandStack.topAndPop();
+                    Double operator1 = operandStack.topAndPop();
+                    if(operator2 != 0)
+                        operandStack.push(operator1 / operator2);
+                    else
+                        throw new DivideByZeroException();
+                }
+            }
+        }
+        return operandStack.topAndPop();
+    }
 }
