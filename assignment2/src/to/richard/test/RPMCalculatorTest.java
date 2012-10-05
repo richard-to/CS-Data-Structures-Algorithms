5a49f04ef817ec65c6323d3a99304afb13b418a6
richard-to <gohn76@gmail.com>
2012-10-05 14:43:45 -0800

    Add tests for stack underflow

--- assignment2/src/to/richard/test/RPMCalculatorTest.java
+++ assignment2/src/to/richard/test/RPMCalculatorTest.java
@@ -13,27 +13,55 @@ public class RPMCalculatorTest {
     public void testValidSyntax(){
         String input = " 1 3 + 4 5 * / 45   sqrt -";
         RPMValidator validator = new RPMValidator();
-        assertEquals(true, validator.validateSyntax(input));
+        assertEquals(false, validator.syntaxError(input));
     }
 
     @Test
     public void testBadSyntax(){
         String input = "1 abd 3 + 4 5 * /";
         RPMValidator validator = new RPMValidator();
-        assertEquals(false, validator.validateSyntax(input));
+        assertEquals(true, validator.syntaxError(input));
     }
 
     @Test
     public void testBadSyntaxNegative(){
         String input = "1 3 + 4 -52 * /";
         RPMValidator validator = new RPMValidator();
-        assertEquals(false, validator.validateSyntax(input));
+        assertEquals(true, validator.syntaxError(input));
     }
 
     @Test
     public void testValidStackUnderflow(){
         String input = "1 3 + 5 -";
         RPMValidator validator = new RPMValidator();
-        assertEquals(true, validator.validateStackUnderflow(input));
+        assertEquals(false, validator.stackUnderflow(input));
+    }
+
+    @Test
+    public void testValidStackUnderflowSqrt(){
+        String input = "1 3 + 5 sqrt sqrt -";
+        RPMValidator validator = new RPMValidator();
+        assertEquals(false, validator.stackUnderflow(input));
+    }
+
+    @Test
+    public void testValidStackUnderflowSingleOperand(){
+        String input = "15";
+        RPMValidator validator = new RPMValidator();
+        assertEquals(false, validator.stackUnderflow(input));
+    }
+
+    @Test
+    public void testValidStackUnderflowComplexExpression(){
+        String input = "11 22 33 44 55 66 77 88 + - sqrt * / / sqrt * +";
+        RPMValidator validator = new RPMValidator();
+        assertEquals(false, validator.stackUnderflow(input));
+    }
+
+    @Test
+    public void testStackUnderflowError(){
+        String input = "11 22 + /";
+        RPMValidator validator = new RPMValidator();
+        assertEquals(true, validator.stackUnderflow(input));
     }
 }
