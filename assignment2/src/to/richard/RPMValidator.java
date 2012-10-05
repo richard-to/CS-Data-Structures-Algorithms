1e8b3a5015bb0f0b654eb53f91dce41f44769b62
richard-to <gohn76@gmail.com>
2012-10-05 14:15:40 -0800

    Add syntax check method

--- assignment2/src/to/richard/RPMValidator.java
+++ assignment2/src/to/richard/RPMValidator.java
@@ -4,4 +4,21 @@ package to.richard;
  * Validates RPM Input
  */
 public class RPMValidator {
+
+    /**
+     * Validate syntax of expression
+     *
+     * @param input
+     * @return boolean
+     */
+    public boolean validateSyntax(String input){
+        String[] tokens = input.split(" ");
+        for(String token:tokens){
+            String trimmedToken = token.trim();
+            if(!trimmedToken.isEmpty() && !trimmedToken.matches("(sqrt|[\\-\\+\\*/]{1}|[0-9]+)")){
+                return false;
+            }
+        }
+        return true;
+    }
 }
