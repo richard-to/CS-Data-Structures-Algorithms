d3adfa1d4e1b83b93097594c5e0f84f93dec5777
richard-to <gohn76@gmail.com>
2012-10-19 10:49:32 -0800

    Add queue tests

--- assignment3/src/to/richard/test/QueueTest.java
+++ assignment3/src/to/richard/test/QueueTest.java
@@ -1,4 +1,45 @@
 package to.richard.test;
 
+import org.junit.Test;
+import to.richard.Queue;
+
+import static org.junit.Assert.assertEquals;
+
 public class QueueTest {
+
+    @Test
+    public void testEnqueueAndDequeue() {
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(1);
+        assertEquals(new Integer(1), queue.dequeue());
+    }
+
+    @Test
+    public void testDequeue() {
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(1).enqueue(2);
+        assertEquals(new Integer(1), queue.dequeue());
+        assertEquals(new Integer(2), queue.dequeue());
+        assertEquals(true, queue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        Queue<Integer> queue = new Queue<Integer>();
+        assertEquals(true, queue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyNot() {
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(1);
+        assertEquals(false, queue.isEmpty());
+    }
+
+    @Test
+    public void testMakeEmpty() {
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(1).enqueue(2).enqueue(3).makeEmpty();
+        assertEquals(true, queue.isEmpty());
+    }
 }
