fb0c396ea3625b895790953211ca14fb789de545
richard-to <gohn76@gmail.com>
2012-10-19 10:49:25 -0800

    Add queue implementation

--- assignment3/src/to/richard/Queue.java
+++ assignment3/src/to/richard/Queue.java
@@ -1,4 +1,50 @@
 package to.richard;
 
-public class Queue {
+/**
+ * Generic Queue Data structure that uses
+ * double linked list internally
+ * @param <E>
+ */
+public class Queue<E> {
+
+    private LinkedList<E> list;
+
+    public Queue() {
+        list = new LinkedList<E>();
+    }
+
+    /**
+     * Checks if queue is empty
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    /**
+     * Adds object to queue
+     * @param <E> object
+     * @return Queue<E>
+     */
+    public Queue<E> enqueue(E object) {
+        list.addFirst(object);
+        return this;
+    }
+
+    /**
+     * Gets oldest object from queue and removes
+     * @return <E> object
+     */
+    public E dequeue(){
+        return list.removeLast();
+    }
+
+    /**
+     * Removes all objects from queue
+     * @return Queue<E>
+     */
+    public Queue<E> makeEmpty() {
+        list = new LinkedList<E>();
+        return this;
+    }
 }
