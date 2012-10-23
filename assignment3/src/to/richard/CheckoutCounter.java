2d4208fa89f809914d6897b95095f164ff7479be
richard-to <gohn76@gmail.com>
2012-10-22 20:39:36 -0800

    Add implementations for various checkout methods

--- assignment3/src/to/richard/CheckoutCounter.java
+++ assignment3/src/to/richard/CheckoutCounter.java
@@ -5,25 +5,79 @@ package to.richard;
  */
 public class CheckoutCounter {
     Queue<Customer> queue;
+    LinkedList<Customer> list;
+    Customer customer;
 
     public CheckoutCounter(){
         queue = new Queue<Customer>();
+        list = new LinkedList<Customer>();
     }
 
     /**
      * Adds a customer to checkout counter
      * @param customer
-     * @return
+     * @return CheckCounter
      */
     public CheckoutCounter addCustomer(Customer customer){
         queue.enqueue(customer);
+        list.add(customer);
         return this;
     }
 
     /**
      * Checks the number of customers in line
+     * @return int
      */
     public int customersInLine(){
         return queue.currentSize();
+    }
+
+    /**
+     * Checks if checkout line is empty
+     * @return boolean
+     */
+    public boolean isEmpty(){
+        return customersInLine() == 0 ? true : false;
+    }
+
+    /**
+     * Increments customer wait time
+     * @return CheckoutCounter
+     */
+    public CheckoutCounter increaseWaitingTime(){
+        Iterator<Customer> iter = list.iterator();
+        while(iter.hasNext()){
+            Customer customer = iter.next();
+            customer.increaseWaitTime();
+        }
+        return this;
+    }
+
+    /**
+     * Processes customer at front of line
+     * @return CheckoutCounter
+     */
+    public CheckoutCounter processCustomer() {
+        if(customer == null && !queue.isEmpty()){
+            customer = queue.dequeue();
+        }
+
+        if(customer != null){
+            customer.process();
+            if(customer.isFinished()){
+                list.remove(customer);
+                customer = null;
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     * Gets current customer
+     * @return Customer
+     */
+    public Customer getCurrentCustomer(){
+        return customer;
     }
 }
