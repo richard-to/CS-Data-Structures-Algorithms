c4015d5becf1553d037b6483c01518f66ac85ce5
richard-to <gohn76@gmail.com>
2012-11-03 21:59:34 -0800

    Add main program for sorting

--- assignment4/src/Assignment4.java
+++ assignment4/src/Assignment4.java
@@ -1,10 +1,40 @@
 import to.richard.*;
 
+import java.io.IOException;
+
 /**
  * Main application class
  */
 public class Assignment4 {
-    public static void main(String[] args){
 
+    private static final String WORDS_FILE = "words.txt";
+    private static final int MAX_TOKENS = 1000;
+
+    public static void main(String[] args) throws IOException {
+        String[] tokens = TextTokenizer.tokenizeFile(WORDS_FILE, MAX_TOKENS);
+
+        sortTest("Insertion Sort Test", tokens, new InsertionSort<String>());
+        sortTest("Selection Sort Test", tokens, new SelectionSort<String>());
+        sortTest("Merge Sort Test", tokens, new MergeSort<String>());
+        sortTest("Quick Sort Test", tokens, new QuickSort<String>());
+    }
+
+    public static void sortTest(String title, String[] tokens, ISort sorter) {
+        System.out.println(title);
+        System.out.println("----------------------------");
+        System.out.println("Original Order");
+        TextTokenizer.print(tokens);
+        System.out.println();
+
+        System.out.println("First Sort");
+        String[] copy = TextTokenizer.copy(tokens);
+        sorter.sort(copy);
+        TextTokenizer.print(copy);
+        System.out.println();
+
+        System.out.println("Second Sort");
+        sorter.sort(copy);
+        TextTokenizer.print(copy);
+        System.out.println();
     }
 }
