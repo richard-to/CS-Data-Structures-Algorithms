165ac872768bed332345438d761cfa93a782ac16
richard-to <gohn76@gmail.com>
2012-11-16 16:52:45 -0900

    Add avl node implementation

--- assignment5/src/to/richard/AvlNode.java
+++ assignment5/src/to/richard/AvlNode.java
@@ -1,4 +1,18 @@
 package to.richard;
 
-public class AvlNode {
+public class AvlNode<E> {
+    public Comparable<E> value;
+    public AvlNode<E> left;
+    public AvlNode<E> right;
+    public int height;
+
+    public AvlNode(Comparable<E> value){
+        this(value, null,null);
+    }
+
+    public AvlNode(Comparable<E> value, AvlNode<E> left, AvlNode<E> right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
 }
