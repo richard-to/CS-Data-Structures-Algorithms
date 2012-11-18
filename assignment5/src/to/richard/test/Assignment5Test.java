d47311e88aab5a10853c75d1b7997df24188ae96
richard-to <gohn76@gmail.com>
2012-11-18 10:56:09 -0900

    Add tests for various search implementations

--- assignment5/src/to/richard/test/Assignment5Test.java
+++ assignment5/src/to/richard/test/Assignment5Test.java
@@ -2,18 +2,25 @@ package to.richard.test;
 
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
+import to.richard.AvlSearch;
+import to.richard.BinarySearch;
+import to.richard.BstSearch;
+import to.richard.LinearSearch;
 import to.richard.MockNumGen;
+import to.richard.NumGen;
 import to.richard.Queue;
 
 public class Assignment5Test {
+
     @Test
     public void testNumGenUnique() {
         Queue<Integer> queue = new Queue<Integer>();
         queue.enqueue(5).enqueue(8).enqueue(5).enqueue(3).enqueue(9).enqueue(8).enqueue(10);
         MockNumGen numGen = new MockNumGen(10, 5, true, queue);
-        int[] sequence = numGen.generate();
-        int[] expected = new int[]{5,8,3,9,10};
+        Integer[] sequence = numGen.generate();
+        Integer[] expected = new Integer[]{5,8,3,9,10};
         assertArrayEquals(expected, sequence);
     }
 
@@ -22,8 +29,55 @@ public class Assignment5Test {
         Queue<Integer> queue = new Queue<Integer>();
         queue.enqueue(5).enqueue(8).enqueue(5).enqueue(3).enqueue(9).enqueue(8).enqueue(10);
         MockNumGen numGen = new MockNumGen(10, 5, false, queue);
-        int[] sequence = numGen.generate();
-        int[] expected = new int[]{5,8,5,3,9};
+        Integer[] sequence = numGen.generate();
+        Integer[] expected = new Integer[]{5,8,5,3,9};
         assertArrayEquals(expected, sequence);
+    }
+
+    @Test
+    public void testMax(){
+        NumGen numGen = new NumGen(2000, 20, true);
+        Integer[] randSeq = numGen.generate();
+        assertEquals(20, randSeq.length);
+    }
+
+    @Test
+    public void testLinearSearch(){
+        Integer[] seq = {233, 23, 78, 34, 80, 822, 2};
+        LinearSearch<Integer> search = new LinearSearch<Integer>(seq);
+        for(int i = 0; i < seq.length; i++){
+            assertEquals(seq[i], search.find(seq[i]));
+        }
+        assertEquals(null, search.find(new Integer(755)));
+    }
+
+    @Test
+    public void testBinarySearch(){
+        Integer[] seq = {233, 23, 78, 34, 80, 822, 2};
+        BinarySearch<Integer> search = new BinarySearch<Integer>(seq);
+        for(int i = 0; i < seq.length; i++){
+            assertEquals(seq[i], search.find(seq[i]));
+        }
+        assertEquals(null, search.find(new Integer(755)));
+    }
+
+    @Test
+    public void testBstSearch(){
+        Integer[] seq = {233, 23, 78, 34, 80, 822, 2};
+        BstSearch<Integer> search = new BstSearch<Integer>(seq);
+        for(int i = 0; i < seq.length; i++){
+            assertEquals(seq[i], search.find(seq[i]));
+        }
+        assertEquals(null, search.find(new Integer(755)));
+    }
+
+    @Test
+    public void testAvlSearch(){
+        Integer[] seq = {233, 23, 78, 34, 80, 822, 2};
+        AvlSearch<Integer> search = new AvlSearch<Integer>(seq);
+        for(int i = 0; i < seq.length; i++){
+            assertEquals(seq[i], search.find(seq[i]));
+        }
+        assertEquals(null, search.find(new Integer(755)));
     }
 }
