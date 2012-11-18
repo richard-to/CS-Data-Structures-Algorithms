eff702350a60eb2752a686bc0397fa5342e45cb3
richard-to <gohn76@gmail.com>
2012-11-18 10:55:34 -0900

    Add bst search

---
+++ assignment5/src/to/richard/BstSearch.java
@@ -0,0 +1,30 @@
+package to.richard;
+
+/**
+ * Find element using binary search tree
+ */
+public class BstSearch<E> extends Search<E> {
+    BinaryTree<E> bst;
+
+    /**
+     * Constructor
+     * @param sequence Array of elements in any order
+     */
+    public BstSearch(Comparable<E>[] sequence){
+        this.bst = new BinaryTree<E>();
+        for(int i = 0; i < sequence.length; i++){
+            this.bst.insert(sequence[i]);
+        }
+    }
+
+    /**
+     * Finds element using bst
+     * Returns null if not found
+     * @param element
+     * @return E
+     */
+    public E find(Comparable<E> element){
+        E foundElement = bst.find(element);
+        return foundElement;
+    }
+}
