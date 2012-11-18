fac4c0ffe7ce32e3df8e556b507e36f2d104ab3b
richard-to <gohn76@gmail.com>
2012-11-18 10:55:39 -0900

    Add avl search

---
+++ assignment5/src/to/richard/AvlSearch.java
@@ -0,0 +1,30 @@
+package to.richard;
+
+/**
+ * Find element using avl tree
+ */
+public class AvlSearch<E> extends Search<E> {
+    AvlTree<E> avl;
+
+    /**
+     * Constructor
+     * @param sequence Array of elements in any order
+     */
+    public AvlSearch(Comparable<E>[] sequence){
+        this.avl = new AvlTree<E>();
+        for(int i = 0; i < sequence.length; i++){
+            this.avl.insert(sequence[i]);
+        }
+    }
+
+    /**
+     * Finds element using avl tree
+     * Returns null if not found
+     * @param element
+     * @return E
+     */
+    public E find(Comparable<E> element){
+        E foundElement = avl.find(element);
+        return foundElement;
+    }
+}
