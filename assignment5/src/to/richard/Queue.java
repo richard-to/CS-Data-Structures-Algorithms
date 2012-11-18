f94edd61f57168db59e0a8ed9639ab9d25792277
richard-to <gohn76@gmail.com>
2012-11-17 17:02:31 -0900

    Add queue and linked list impl from previous assignments

---
+++ assignment5/src/to/richard/Queue.java
@@ -0,0 +1,58 @@
+package to.richard;
+
+/**
+ * Generic Queue Data structure that uses
+ * double linked list internally
+ * @param <E>
+ */
+public class Queue<E> {
+
+    private LinkedList<E> list;
+
+    public Queue() {
+        list = new LinkedList<E>();
+    }
+
+    /**
+     * Checks if queue is empty
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    /**
+     * Adds object to queue
+     * @param <E> object
+     * @return Queue<E>
+     */
+    public Queue<E> enqueue(E object) {
+        list.addFirst(object);
+        return this;
+    }
+
+    /**
+     * Gets oldest object from queue and removes
+     * @return <E> object
+     */
+    public E dequeue(){
+        return list.removeLast();
+    }
+
+    /**
+     * Removes all objects from queue
+     * @return Queue<E>
+     */
+    public Queue<E> makeEmpty() {
+        list = new LinkedList<E>();
+        return this;
+    }
+
+    /**
+     * Gets current size of queue()
+     * @return int
+     */
+    public int currentSize() {
+        return list.size();
+    }
+}
