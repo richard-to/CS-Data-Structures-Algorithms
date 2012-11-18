90b10b3816ee6a654502e4714b507b6ac78222bb
richard-to <gohn76@gmail.com>
2012-11-18 10:55:29 -0900

    Add binary search

---
+++ assignment5/src/to/richard/BinarySearch.java
@@ -0,0 +1,51 @@
+package to.richard;
+
+/**
+ * Uses binary search to find element
+ */
+public class BinarySearch<E> extends Search<E> {
+
+    Comparable<E>[] sequence;
+
+    /**
+     * Constructor
+     * @param sequence Array of elements in any order
+     */
+    public BinarySearch(Comparable<E>[] sequence){
+        QuickSort<E> quickSort = new QuickSort<E>();
+        this.sequence = (Comparable<E>[])quickSort.sort(sequence);
+    }
+
+    /**
+     * Finds element using binary search
+     * Returns null if not found
+     * @param element
+     * @return E
+     */
+    public E find(Comparable<E> element){
+        Comparable<E> foundElement = find(element, sequence, 0, sequence.length-1);
+        return (foundElement != null) ? (E)foundElement : null;
+    }
+
+    /**
+     * Find element using recursive binary search
+     * @param element
+     * @param sequence
+     * @param min Inclusive
+     * @param max Inclusive
+     * @return Comparable<E>;
+     */
+    private Comparable<E> find(Comparable<E> element, Comparable<E>[] sequence, int min, int max){
+        int mid = max - ((max - min) / 2);
+        int elementVsSequence = element.compareTo((E)sequence[mid]);
+        if(elementVsSequence == 0){
+            return element;
+        } else if(max == min) {
+            return null;
+        } if(elementVsSequence > 0){
+            return find(element, sequence, mid+1, max);
+        } else {
+            return find(element, sequence, min, mid-1);
+        }
+    }
+}
