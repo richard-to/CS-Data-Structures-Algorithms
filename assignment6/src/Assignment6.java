0634aa80f572772f9778c413bbdc24c9de178148
richard-to <gohn76@gmail.com>
2012-12-04 22:41:05 -0900

    Add print matrix and generate random adjacency matrix

--- assignment6/src/Assignment6.java
+++ assignment6/src/Assignment6.java
@@ -1,5 +1,51 @@
-public class Assignment6 {
-    public static void main(String[] args) {
+import java.util.Random;
 
+/**
+ * Alaska Travel Agent Problem
+ */
+public class Assignment6 {
+
+    public static final int MATRIX_SIZE = 3;
+    public static final int MIN_PRICE = 99;
+    public static final int MAX_PRICE = 1499;
+
+    /**
+     * Main Program
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[][] matrix = buildMatrix(MATRIX_SIZE, MIN_PRICE, MAX_PRICE);
+        printMatrix(matrix);
+    }
+
+    /**
+     * Builds price matrix
+     * @param size
+     * @param minPrice
+     * @param maxPrice
+     * @return price matrix
+     */
+    public static int[][] buildMatrix(int size, int minPrice, int maxPrice) {
+        Random rand = new Random();
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int g = 0; g < size; g++) {
+                matrix[i][g] = rand.nextInt(maxPrice - minPrice + 1) + minPrice;
+            }
+        }
+        return matrix;
+    }
+
+    /**
+     * Prints matrix
+     * @param matrix
+     */
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int g = 0; g < matrix[i].length; g++) {
+                System.out.printf("%4d ",matrix[i][g]);
+            }
+            System.out.println();
+        }
     }
 }
